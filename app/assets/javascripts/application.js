3177cdcc09aa714e46737e1299914e6affc770d0
Gaylen Miller <gaylenmiller@gmail.com>
2015-03-13 23:40:54 -0700

    update the menu partial

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,9 @@
 // about supported directives.
 //
 //= require jquery
+//= require bootstrap-sprockets
 //= require jquery_ujs
 //= require turbolinks
-// Require Bootstrap Javascripts in app/assets/javascripts/application.js:
-//= require bootstrap-sprockets
+
 //= require_tree .
 
